80aaec3ef606cac5aa8e487c9919cff5ca33889c
naveen2493 <naveenkumarsadasivam93@gmail.com>
2020-09-03 20:57:30 +0530

    give timeout

--- src/app/thanks/thanks.page.ts
+++ src/app/thanks/thanks.page.ts
@@ -13,9 +13,9 @@ export class ThanksPage implements OnInit {
   constructor(private router: Router) { }
 
   ngOnInit() {
-    // setTimeout(() => {
-    //   this.router.navigate(['/home']);
-    // }, 5000);
+    setTimeout(() => {
+      this.router.navigate(['/home']);
+    }, 5000);
   }
 
   tapEvent() {
